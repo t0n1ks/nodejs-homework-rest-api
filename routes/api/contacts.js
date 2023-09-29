035c63448bdfdc550c93143e31886cefd8c02972
Nikita Titov <nikitatitov255@gmail.com>
2023-09-30 01:30:45 +0300

    fix 0.0.1
    доробленно, тепер все працює та відображає як у вашому прикладі

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,29 +1,62 @@
 const express = require('express');
 const router = express.Router();
+const fs = require('fs/promises');
 const { v4: uuidv4 } = require('uuid'); 
 
-let contacts = [];
+const { createUserDataValidator } = require('../../utils/userValidators');
 
-router.get('/', (req, res) => {
-  res.status(200).json({ contacts });
+const readContacts = async () => {
+  try {
+    const data = await fs.readFile('contacts.json', 'utf8');
+    return JSON.parse(data);
+  } catch (error) {
+    return [];
+  }
+};
+
+const writeContacts = async (data) => {
+  await fs.writeFile('contacts.json', JSON.stringify(data, null, 2), 'utf8');
+};
+
+router.get('/', async (req, res) => {
+  const contacts = await readContacts();
+  res.status(200).json( contacts );
 });
 
-router.get('/:contactId', (req, res) => {
+router.get('/:contactId', async (req, res) => {
   const { contactId } = req.params;
+  const contacts = await readContacts();
   const contact = contacts.find((c) => c.id === contactId);
 
   if (!contact) {
     return res.status(404).json({ message: 'Not found' });
   }
 
-  res.status(200).json({ contact });
+  res.status(200).json( contact );
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   const { name, email, phone } = req.body;
+  const missingFields = [];
 
-  if (!name || !email || !phone) {
-    return res.status(400).json({ message: 'Missing required fields' });
+  if (!name) {
+    missingFields.push('name');
+  }
+  if (!email) {
+    missingFields.push('email');
+  }
+  if (!phone) {
+    missingFields.push('phone');
+  }
+
+  if (missingFields.length > 0) {
+    return res.status(400).json({ message: `Missing required ${missingFields.join(', ')} fields` });
+  }
+
+  const validationResult = createUserDataValidator(req.body);
+
+  if (validationResult.error) {
+    return res.status(400).json({ message: validationResult.error.details[0].message });
   }
 
   const newContact = {
@@ -33,12 +66,18 @@ router.post('/', (req, res) => {
     phone,
   };
 
+  const contacts = await readContacts();
   contacts.push(newContact);
-  res.status(201).json({ contact: newContact });
+
+  await writeContacts(contacts);
+
+  res.status(201).json(newContact);
 });
 
-router.delete('/:contactId', (req, res) => {
+
+router.delete('/:contactId', async (req, res) => {
   const { contactId } = req.params;
+  const contacts = await readContacts();
   const index = contacts.findIndex((c) => c.id === contactId);
 
   if (index === -1) {
@@ -46,27 +85,52 @@ router.delete('/:contactId', (req, res) => {
   }
 
   contacts.splice(index, 1);
+
+  await writeContacts(contacts);
+
   res.status(200).json({ message: 'Contact deleted' });
 });
 
-router.put('/:contactId', (req, res) => {
+router.put('/:contactId', async (req, res) => {
   const { contactId } = req.params;
   const { name, email, phone } = req.body;
+  const contacts = await readContacts();
   const index = contacts.findIndex((c) => c.id === contactId);
 
   if (index === -1) {
     return res.status(404).json({ message: 'Not found' });
   }
 
-  if (!name && !email && !phone) {
-    return res.status(400).json({ message: 'Missing fields' });
+  const missingFields = [];
+
+  if (!name) {
+    missingFields.push('name');
+  }
+  if (!email) {
+    missingFields.push('email');
+  }
+  if (!phone) {
+    missingFields.push('phone');
+  }
+
+  if (missingFields.length > 0) {
+    return res.status(400).json({ message: `Missing required ${missingFields.join(', ')} fields` });
+  }
+
+  const validationResult = createUserDataValidator(req.body);
+
+  if (validationResult.error) {
+    return res.status(400).json({ message: validationResult.error.details[0].message });
   }
 
   if (name) contacts[index].name = name;
   if (email) contacts[index].email = email;
   if (phone) contacts[index].phone = phone;
 
-  res.status(200).json({ contact: contacts[index] });
+  await writeContacts(contacts);
+
+  res.status(200).json(contacts[index]);
 });
+
 
 module.exports = router;
--- utils/userValidators.js
+++ utils/userValidators.js
@@ -1,15 +1,3 @@
-// const Joi = require ('joi');
-
-// exports.createUserDataValidator = (data) =>
-// Joi
-// .object()
-// .options({abortEarly: false})
-// .keys({
-//     name: Joi.string().min(3).max(12).required(),
-//     year: Joi.number().min(1920).max(2230),
-// })
-// .validate(data);
-
 const Joi = require('joi');
 
 exports.createUserDataValidator = (data) =>
